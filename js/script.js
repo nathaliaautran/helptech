a128cf83bbd1d20b4c964d1681f3d894b62d870f
Nathalia <63376794+nathaliaautran@users.noreply.github.com>
2023-10-26 21:39:45 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+function mostrarCards(tecnologia) {
+    var cards = document.querySelectorAll('.plano-estudo');
+    cards.forEach(function(card) {
+        if (card.id === tecnologia) {
+            if (card.classList.contains('show')) {
+                card.classList.remove('show');
+            } else {
+                card.classList.add('show');
+            }
+        } else {
+            card.classList.remove('show');
+        }
+    });
+}
+
+function checkVisibility() {
+    const elements = document.querySelectorAll('.button-tec');
+    elements.forEach((element) => {
+        const elementTop = element.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        if (elementTop < windowHeight) {
+            element.style.opacity = 1; // Quando o elemento estiver visível, defina a opacidade como 1
+            element.classList.add('animate-button-tec'); // Adiciona a classe para iniciar a animação
+        }
+    });
+}
+
+window.addEventListener('scroll', checkVisibility);
